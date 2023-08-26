5da2caf877c130cc356baa18195af8faa509c88f
chandana-kotla <kchandana1869@gmail.com>
2023-08-26 11:28:41 +0530

    html practice

--- ReactJs/my-app/src/UserApp/UserDetails.jsx
+++ ReactJs/my-app/src/UserApp/UserDetails.jsx
@@ -14,7 +14,7 @@ const UserDetails = (props) => {
                         <div className="card-body">
                             <ul className="list-group">
                                 <li className="list-group-item">{props.selectedUser.age}</li>
-                                <li className="list-group-item">{props.selectedUser.bloodGroup}</li>
+                                <li className="list-group-item">{props.selectedUser.firstName}</li>
                             </ul>
                         </div>
                     </div>
